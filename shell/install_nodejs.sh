903b4379cc522d7c5d0adc6fd445e4f94d0669ef
kasamatsu <ys@spluss.biz>
2019-10-27 21:59:17 +0900

    latestの場合の記述をコメントとして追加

--- shell/install_nodejs.sh
+++ shell/install_nodejs.sh
@@ -5,7 +5,8 @@ apt-get upgrade -y
 apt-get install nodejs npm -y
 npm cache clean
 npm install n -g
-n stable
+n stable #安定版
+#n latest #最新版
 ln -sf /usr/local/bin/node /usr/bin/node
 apt-get purge -y nodejs npm
 echo "install nodejs complete."
